4cc8c2337a88fbd570c409ac22c316a36fc5363f
PrajwolNeupane <prajwolneupane68@gmail.com>
2023-05-29 12:54:30 +0545

    Contact Page UI Done

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import LoginPage from './Page/LoginPage';
 import { Routes,Route } from 'react-router-dom';
 import HomePage from './Page/HomePage';
 import LayOut from './Component/Layout/Layout';
+import ContactPage from './Page/ContactPage';
 const SignupPage = lazy(() => import('./Page/SignupPage'));
 
 
@@ -12,6 +13,7 @@ function App() {
     <Routes>
       <Route path='/' element={<LayOut/>}>
       <Route index element={<HomePage />}/>
+      <Route path='/contact' element={<ContactPage />}/>
       </Route>
      <Route path='/log-in' element={ <LoginPage />}/>
      <Route path='/sign-up' element={ <Suspense fallback="Loading..."><SignupPage /></Suspense>}/>
